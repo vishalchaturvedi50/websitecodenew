6cc246d398ddb1181bc3b34615d7b5c1ae3fc6f6
Vishal Chaturvedi <vishalchaturvedi50@gmail.com>
2019-02-16 23:51:51 +0530

    tech skill section

--- src/app/techskill/techskill.component.ts
+++ src/app/techskill/techskill.component.ts
@@ -12,4 +12,23 @@ export class TechskillComponent implements OnInit {
   ngOnInit() {
   }
 
+  public techImages = [
+    { img: "../../assets/tech/html.png", desc: "" },
+    { img: "../../assets/tech/css.png", desc: "" },
+    { img: "../../assets/tech/js.png", desc: "" },
+    { img: "../../assets/tech/git.png", desc: "" },
+    { img: "../../assets/tech/angular.png", desc: "" },
+    { img: "../../assets/tech/typescript.png", desc: "" },
+    { img: "../../assets/tech/sass.png", desc: "" },
+    { img: "../../assets/tech/bootstrap.png", desc: "" },
+    { img: "../../assets/tech/materialdesign.png", desc: "" },
+    { img: "../../assets/tech/predix.png", desc: "" },
+    { img: "../../assets/tech/highcharts.png", desc: "" },
+    { img: "../../assets/tech/d3.png", desc: "" },
+    { img: "../../assets/tech/java.png", desc: "" },
+    { img: "../../assets/tech/mongo.png", desc: "" },
+    { img: "../../assets/tech/pg.png", desc: "" },
+
+  ]
+
 }
