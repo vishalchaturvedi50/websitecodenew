e650bf7e6d3988e0deba4aaa8c3d622b9228455e
Vishal Chaturvedi <vishalchaturvedi50@gmail.com>
2019-02-17 19:09:15 +0530

    education

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -12,4 +12,22 @@ export class EducationComponent implements OnInit {
   ngOnInit() {
   }
 
+  public education = [{
+    title: "Bachelor of Technology | Hindustan Institute of Technology and Management, UPTU",
+    duration: "August 2010 - May 2014",
+    chipsData: [
+      "Head college cultural society", "Drummer",
+      "Member and later head of event management group",
+      "Campus Ambassador TechKriti'14 IIT Kanpur"
+    ]
+  },
+  {
+    title: "Senior Secondary & Secondary Examinations | M.M. Sr. Sec. Public School CBSE",
+    duration: "April 2007 - March 2010",
+    chipsData: [
+      "Head music and event club", "Member Debate Club "
+    ]
+  }
+  ]
+
 }
