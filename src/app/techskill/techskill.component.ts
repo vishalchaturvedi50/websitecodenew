d1c09d5637fdde8f1f87802cdb4fe6e0ac0df617
Vishal Chaturvedi <vishalchaturvedi50@gmail.com>
2019-02-24 18:12:32 +0530

    tech skills in common file

--- src/app/techskill/techskill.component.ts
+++ src/app/techskill/techskill.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { techSkillsConst } from 'src/environments/environment';
 
 @Component({
   selector: 'app-techskill',
@@ -7,28 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TechskillComponent implements OnInit {
 
+  public techImages = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.techImages = techSkillsConst;
   }
 
-  public techImages = [
-    { img: "../../assets/tech/html.png", desc: "" },
-    { img: "../../assets/tech/css.png", desc: "" },
-    { img: "../../assets/tech/js.png", desc: "" },
-    { img: "../../assets/tech/git.png", desc: "" },
-    { img: "../../assets/tech/angular.png", desc: "" },
-    { img: "../../assets/tech/typescript.png", desc: "" },
-    { img: "../../assets/tech/sass.png", desc: "" },
-    { img: "../../assets/tech/bootstrap.png", desc: "" },
-    { img: "../../assets/tech/materialdesign.png", desc: "" },
-    { img: "../../assets/tech/predix.png", desc: "" },
-    { img: "../../assets/tech/highcharts.png", desc: "" },
-    { img: "../../assets/tech/d3.png", desc: "" },
-    { img: "../../assets/tech/java.png", desc: "" },
-    { img: "../../assets/tech/mongo.png", desc: "" },
-    { img: "../../assets/tech/pg.png", desc: "" },
-
-  ]
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -28,6 +28,24 @@ export const aboutMeConst = {
 traveling is my new passion. If you are interested to know more about my work or even if you are interested to know
 my traveling stories (not many though) you can reach me out over email.`
   ]
-}
+};
+
+export const techSkillsConst = [
+  { img: "./../assets/tech/html.png", desc: "" },
+  { img: "./../assets/tech/css.png", desc: "" },
+  { img: "./../assets/tech/js.png", desc: "" },
+  { img: "./../assets/tech/git.png", desc: "" },
+  { img: "./../assets/tech/angular.png", desc: "" },
+  { img: "./../assets/tech/typescript.png", desc: "" },
+  { img: "./../assets/tech/sass.png", desc: "" },
+  { img: "./../assets/tech/bootstrap.png", desc: "" },
+  { img: "./../assets/tech/materialdesign.png", desc: "" },
+  { img: "./../assets/tech/predix.png", desc: "" }, 
+  { img: "./../assets/tech/highcharts.png", desc: "" },
+  { img: "./../assets/tech/d3.png", desc: "" },
+  { img: "./../assets/tech/java.png", desc: "" },
+  { img: "./../assets/tech/mongo.png", desc: "" },
+  { img: "./../assets/tech/pg.png", desc: "" },
+];
 
 
