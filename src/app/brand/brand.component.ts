bac0c1bcee1f2b2546703ce8eb43431c6c174182
Vishal Chaturvedi <vishalchaturvedi50@gmail.com>
2019-02-24 18:18:55 +0530

    brand in const file

--- src/app/brand/brand.component.ts
+++ src/app/brand/brand.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { brandIconsConst } from 'src/environments/environment';
 
 @Component({
   selector: 'app-brand',
@@ -12,12 +13,6 @@ export class BrandComponent implements OnInit {
   ngOnInit() {
   }
 
-  public icons = [
-    { iconpath: "../../assets/icons/linkedin.png", link: "https://www.linkedin.com/in/vishal-chaturvedi-916b1478/" },
-    { iconpath: "../../assets/icons/github.png", link: "https://github.com/vishalchaturvedi50" },
-    { iconpath: "../../assets/icons/quora.png", link: "https://vishalchaturvedi50.quora.com/" },
-    { iconpath: "../../assets/icons/gmail.png", link: "mailto:vishalchaturvedi50@gmail.com" },
-    { iconpath: "../../assets/icons/facebook.png", link: "https://www.facebook.com/chaturvedi.vishal4" }
-  ]
+  public icons = brandIconsConst;
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -108,3 +108,12 @@ export const hobbyConst = [
   "./../assets/hobby/movies.png",
   "../assets/hobby/book.png",
 ];
+
+
+export const brandIconsConst = [
+  { iconpath: "./../assets/icons/linkedin.png", link: "https://www.linkedin.com/in/vishal-chaturvedi-916b1478/" },
+  { iconpath: "./../assets/icons/github.png", link: "https://github.com/vishalchaturvedi50" },
+  { iconpath: "./../assets/icons/quora.png", link: "https://vishalchaturvedi50.quora.com/" },
+  { iconpath: "./../assets/icons/gmail.png", link: "mailto:vishalchaturvedi50@gmail.com" },
+  { iconpath: "./../assets/icons/facebook.png", link: "https://www.facebook.com/chaturvedi.vishal4" }
+];
